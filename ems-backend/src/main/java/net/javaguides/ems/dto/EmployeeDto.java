a06f379b7be16143950b9a75ba51b0724e4421fe
chrisAfrotech <tchiaguiachristophe25@gmail.com>
2024-01-11 09:14:09 +0100

    Innitialisation of the project with reactjs in frontEnd, spring boot in backEnd, Sql for the database and MySql workbench for the dbms.

---
+++ ems-backend/src/main/java/net/javaguides/ems/dto/EmployeeDto.java
@@ -0,0 +1,20 @@
+package net.javaguides.ems.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
